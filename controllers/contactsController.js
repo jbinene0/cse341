2fb8de714f598b5b49b9a1df20ec94b9fc9ca1a1
Binenej <bin22013@byui.edu>
2026-05-11 01:25:19 -0600

    add POST PUT DELETE routes

--- controllers/contactsController.js
+++ controllers/contactsController.js
@@ -1,4 +1,3 @@
-const { get } = require('mongoose');
 const { getDb } = require('../db/connection');
 const { ObjectId } = require('mongodb');
 
@@ -8,20 +7,74 @@ const getALLContacts = async (req, res) => {
         const contacts = await db.collection('contacts').find().toArray();
         res.status(200).json(contacts);
     } catch (err) {
-        res.status(500).json({ error: err.message });   
+        res.status(500).json({ error: err.message });
     }
-}; 
+};
 
 const getContactById = async (req, res) => {
     try {
         const db = getDb();
-        const contact = await db.collection('contacts').findOne({_id: new ObjectId(req.params.id)});
-        if (!contact) return res.status(404).json({message: 'Contact not found'});
+        const contact = await db.collection('contacts').findOne({ _id: new ObjectId(req.params.id) });
+        if (!contact) return res.status(404).json({ message: 'Contact not found' });
         res.status(200).json(contact);
-    }
-    catch (err) {
+    } catch (err) {
         res.status(500).json({ error: err.message });
     }
-}; 
+};
 
-module.exports = { getALLContacts, getContactById };
+const createContact = async (req, res) => {
+    try {
+        const { firstName, lastName, email, phone, birthday } = req.body;
+
+        if (!firstName || !lastName || !email || !phone || !birthday) {
+            return res.status(400).json({ message: 'All fields are required' });
+        }
+
+        const db = getDb();
+        const result = await db.collection('contacts').insertOne({ firstName, lastName, email, phone, birthday });
+
+        res.status(201).json({ id: result.insertedId });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+const updateContact = async (req, res) => {
+    try {
+        const id = new ObjectId(req.params.id);
+        const { firstName, lastName, email, phone, birthday } = req.body;
+
+        const db = getDb();
+        const result = await db.collection('contacts').replaceOne(
+            { _id: id },
+            { firstName, lastName, email, phone, birthday }
+        );
+
+        if (result.matchedCount === 0) {
+            return res.status(404).json({ message: 'Contact not found' });
+        }
+
+        res.status(204).send();
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+const deleteContact = async (req, res) => {
+    try {
+        const id = new ObjectId(req.params.id);
+
+        const db = getDb();
+        const result = await db.collection('contacts').deleteOne({ _id: id });
+
+        if (result.deletedCount === 0) {
+            return res.status(404).json({ message: 'Contact not found' });
+        }
+
+        res.status(200).json({ message: 'Contact deleted successfully' });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+module.exports = { getALLContacts, getContactById, createContact, updateContact, deleteContact };

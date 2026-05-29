5a27e81ec0b828e5b528e01c9fed8d12c397403b
Binenej <bin22013@byui.edu>
2026-05-29 17:09:29 -0600

    Fix CORS for all methods

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const PORT = process.env.PORT || 8080;
 app.use(express.json());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
 

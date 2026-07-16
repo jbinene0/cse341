2a62d080f2b75d21266f72a96e8fba53914b353e
Binenej <bin22013@byui.edu>
2026-07-15 19:37:14 -0600

    Fix session cookie for production

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
-  cookie: { secure: false },
+  cookie: { secure: process.env.NODE_ENV === 'production' },
 }));
 app.use(passport.initialize());
 app.use(passport.session());
